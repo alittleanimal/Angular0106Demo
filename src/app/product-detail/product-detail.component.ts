9bd2c21a948f1c78900ff378cba5868d92b8f0be
animal <741996133@qq.com>
2018-01-17 01:19:20 +0800

    add comment

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
-import { Product, ProductService, Comment  } from '../shared/product.service';
+import { Product, ProductService, Comment } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,6 +11,10 @@ export class ProductDetailComponent implements OnInit {
 
   comments: Comment[];
   product: Product;
+  newRating: number = 5;
+  newComment: string = '';
+
+  isCommentHidden = true;
 
   constructor(private routeInfo: ActivatedRoute,
     private productService: ProductService) { }
@@ -20,6 +24,18 @@ export class ProductDetailComponent implements OnInit {
 
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment() {
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), 'someOne', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum /this.comments.length;
+
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
   }
 
 }
